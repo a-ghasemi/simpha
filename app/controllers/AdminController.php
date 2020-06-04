dc516fc9cffc2c1dd42ebde78dcd7c30f78d8da8
Armin Ghasemi <teamworks2016@gmail.com>
2020-06-04 13:52:12 +0200

    Removes Test Redirect

--- app/controllers/AdminController.php
+++ app/controllers/AdminController.php
@@ -4,13 +4,11 @@
 namespace App\Controllers;
 
 use Kernel\Controller;
-use Kernel\Redirect;
 use Kernel\View;
 
 class AdminController extends Controller
 {
     protected function get_index(){
-        return Redirect::to("/home");
         return View::show("admin.index");
     }
 }
