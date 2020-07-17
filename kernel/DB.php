94ab78cb1f99bb74e06e85c836a4f1d6202474ff
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-07-18 00:56:23 +0430

    init function

---
+++ kernel/DB.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App;
+
+
+class DB
+{
+
+    private $connection;
+
+    private $server_name;
+    private $server_port;
+    private $username;
+    private $password;
+    private $db_name;
+
+    public $state;
+    public $error;
+    public $on_demand;
+
+    function __construct($server_name, $server_port, $username, $password, $db_name, $on_demand = false)
+    {
+        $this->server_name = $server_name;
+        $this->server_port = $server_port;
+        $this->on_demand = $on_demand;
+
+        $this->db_name = $db_name;
+        $this->username = $username;
+        $this->password = $password;
+
+        $this->error = null;
+        $this->state = 'created';
+    }
+
+    public function connect(){
+        $this->connection = new \mysqli($this->server_name, $this->username, $this->password, $this->db_name);
+
+        if ($this->connection->connect_error) {
+            $this->error = $this->connection->connect_error;
+            $this->state = 'error';
+            return;
+        }
+
+        $this->connection->autocommit(!$this->on_demand);
+        $this->state = 'connected';
+    }
+
+    public function disconnect(){
+        if($this->state !== 'connected') return;
+        $this->connection->close();
+    }
+
+    public function commit(){
+        if($this->on_demand) return true;
+
+        if (!$this->connection->commit()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Transaction commit failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+    public function begin_transaction(){
+        if($this->on_demand) return true;
+
+        if (!$this->connection->begin_transaction()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Begin Transaction failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+    public function rollback(){
+        if($this->on_demand) return false;
+
+        if (!$this->connection->rollback()) {
+//            $this->error = $this->connection->connect_error;
+            $this->error = 'Rollback Transaction failed';
+            $this->state = 'error';
+            return false;
+        }
+
+        return true;
+    }
+
+    public function select($query){
+//        $query = "SELECT id, firstname, lastname FROM MyGuests";
+        $result = $this->connection->query($query);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                yield $row;
+            }
+        }
+        else {
+            return null;
+        }
+    }
+
+}
--- kernel/Kernel.php
+++ kernel/Kernel.php
@@ -2,16 +2,27 @@
 
 namespace Kernel;
 
+use App\DB;
+
 class Kernel
 {
     private $url;
     private $data;
+    private $database;
 
     static $env;
 
     public function __construct($debug_mode = false)
     {
         Self::$env = (new EnvParser(base_path(".env")))->parse();
+
+        $this->connectDatabase(
+            env_get('DB_USER'),
+            env_get('DB_PASS'),
+            env_get('DB_NAME'),
+            env_get('DB_HOST', 'localhost'),
+            env_get('DB_PORT', 3306),
+        );
 
         $this->data["request_type"] = $_SERVER['REQUEST_METHOD'];
 
@@ -20,13 +31,13 @@ class Kernel
 
         //TODO: using HTTP_HOST & REQUEST_URI has security problem, change this as soon as possible
         $url = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-        $url = explode('/',parse_url(strtolower(trim($url)))['path']);
+        $url = explode('/', parse_url(strtolower(trim($url)))['path']);
         array_shift($url);
 
         $tmp = array_shift($url);
-        $this->url['class'] = (!empty($tmp))? $tmp : 'home';
+        $this->url['class'] = (!empty($tmp)) ? $tmp : 'home';
         $tmp = array_shift($url);
-        $this->url['method'] = (!empty($tmp))? $tmp : 'index';
+        $this->url['method'] = (!empty($tmp)) ? $tmp : 'index';
         $this->url['params'] = $url ?? [];
 
         $this->data["url"] = $this->url;
@@ -34,19 +45,28 @@ class Kernel
         $this->data['debug_mode'] = $debug_mode;
     }
 
-    public function run(){
-        $controller = "\\App\\Controllers\\".ucwords($this->url['class']).'Controller';
+    private function connectDatabase($user, $pass, $db_name, $host = 'localhost', $port = '3306')
+    {
+        $this->database = new DB($host, $port, $user, $pass, $db_name);
+        $this->database->connect();
+        if($this->database->error){
+            log("Source Database Connection Failed!");
+        }
+    }
 
-        if(!class_exists($controller)){
-            header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
+    public function run()
+    {
+        $controller = "\\App\\Controllers\\" . ucwords($this->url['class']) . 'Controller';
+
+        if (!class_exists($controller)) {
+            header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found", true, 404);
             return;
         }
-
         $page = new $controller($this->data);
         $ret = $page->run();
 
-        if(is_object($ret)){
-            switch(get_class($ret)){
+        if (is_object($ret)) {
+            switch (get_class($ret)) {
                 case 'Kernel\View':
                     $ret->getContent();
                     break;
@@ -54,15 +74,13 @@ class Kernel
                     $ret->go();
                     break;
                 default:
-                    print("Kernel Error: Class ".get_class($ret)." is not cased yet.");
+                    print("Kernel Error: Class " . get_class($ret) . " is not cased yet.");
             }
-        }
-        elseif(is_string($ret)){
+        } elseif (is_string($ret)) {
             @ob_start();
             print($ret);
             @ob_flush();
-        }
-        elseif(empty($ret)){
+        } elseif (empty($ret)) {
             print('Kernel Warning: Content is Empty!');
         }
 
