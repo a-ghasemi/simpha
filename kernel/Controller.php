06dae6a5dc635790f29920eab2c97b5dc09a8c80
Armin Ghasemi <armin.ghassemi@gmail.com>
2020-07-19 16:39:04 +0200

    Adds new items in routing

--- kernel/Controller.php
+++ kernel/Controller.php
@@ -20,6 +20,14 @@ class Controller
         }
 
         if(!method_exists($this,$function)){
+            $function = $this->data['request_type'] . '_';
+        }
+
+        if(!method_exists($this,$function)){
+            $function = 'any_';
+        }
+
+        if(!method_exists($this,$function)){
             header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found", true, 404);
             return;
         }
