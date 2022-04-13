92597667f2dc1d29b973764ecc8bd4b2cca9c834
Armin Ghasemi <datarivers.org@gmail.com>
2022-04-13 21:39:33 +0300

    Repairs migration

--- kernel/Command/Db.php
+++ kernel/Command/Db.php
@@ -8,71 +8,35 @@ use Kernel\ClassMap;
 
 class Db extends AbsCommand
 {
-    public function check()
-    {
-        $this->comment('Database Connection Successfully.');
-    }
+    const NAMESPACE_MIGRATION = 'App\\Database\\Migrations';
 
     public function migrate()
     {
-        $namespace = 'App\\Database\\Migrations';
-        $migration_classes = ClassMap::map($namespace, app_path('Database/Migrations'));
+        $migration_classes = ClassMap::map(self::NAMESPACE_MIGRATION, app_path('Database/Migrations'));
 
         foreach ($migration_classes as $class => $methods) {
-            $obj = $namespace . "\\" . $class;
+            $obj = self::NAMESPACE_MIGRATION . "\\" . $class;
             $obj = new $obj($this->db_connection, $this->error_handler);
             $obj->up();
             $this->comment("Table [$class] Created Successfully.");
-        }
-
-    }
-
-    public function info()
-    {
-        $namespace = 'App\\Database\\Migrations';
-        $migration_classes = ClassMap::map($namespace, app_path('database/migrations'));
-        foreach ($migration_classes as $class => $methods) {
-            $obj = $namespace . "\\" . $class;
-            $obj = new $obj($this->db_connection);
-            $obj->up();
-            $this->comment("Table [$class] Created Successfully.");
-        }
-
-    }
-
-    public function seed(): void
-    {
-        $folder = $this->parameters[0] ?? '';
-
-        $namespace = 'App\\Database\\Seeds' . ($folder ? "\\$folder" : "");
-        $seed_classes = ClassMap::map($namespace, app_path('Database/Seeds/' . $folder));
-
-        foreach ($seed_classes as $class => $methods) {
-            $obj = $namespace . "\\" . $class;
-            $obj = new $obj($this->db_connection);
-            $obj->run();
-            $this->comment("Seed [$class] Executed Successfully.");
         }
     }
 
     public function rollback()
     {
-        $namespace = 'App\\database\\migrations';
-        $migration_classes = ClassMap::map($namespace, app_path('Database/Migrations'));
+        $migration_classes = ClassMap::map(self::NAMESPACE_MIGRATION, app_path('Database/Migrations'));
         $migration_classes = array_reverse($migration_classes);
         foreach ($migration_classes as $class => $methods) {
-            $obj = $namespace . "\\" . $class;
-            $obj = new $obj;
+            $obj = self::NAMESPACE_MIGRATION . "\\" . $class;
+            $obj = new $obj($this->db_connection);
             $obj->down();
             $this->comment("Table [$class] Dropped Successfully.");
         }
-
     }
 
     public function renew()
     {
         $this->rollback();
         $this->migrate();
-        $this->seed();
     }
 }
